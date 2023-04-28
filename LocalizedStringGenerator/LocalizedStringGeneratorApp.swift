8d225c9629ea27d79e52c1f14052372a076ab135
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2023-04-28 13:06:11 -0300

    Initial Commit

---
+++ LocalizedStringGenerator/LocalizedStringGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedStringGeneratorApp.swift
+//  LocalizedStringGenerator
+//
+//  Created by Marcelo Diefenbach on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedStringGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
